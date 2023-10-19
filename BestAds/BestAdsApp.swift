c8d6f3ee3464c282862149ecc7346db5eb29079b
sagor155670 <sagor@braincraftapps.com>
2023-10-19 16:23:56 +0600

    Initial Commit

---
+++ BestAds/BestAdsApp.swift
@@ -0,0 +1,17 @@
+//
+//  BestAdsApp.swift
+//  BestAds
+//
+//  Created by Saiful Islam Sagor on 19/10/23.
+//
+
+import SwiftUI
+
+@main
+struct BestAdsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
